5b11d34d3ca551fe1c4712e2dee71d569e62a2a6
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-20 15:53:56 +0200

    Removed variable 'code' and reduced number of lines

--- hsh.c
+++ hsh.c
@@ -15,7 +15,7 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 	ssize_t read = 0;
 	list_t *path_list = NULL;
 	char *arguments[4096];
-	int code = 0, status = 0;
+	int status = 0;
 
 	path_list = create_path_list(env);
 
@@ -25,28 +25,29 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 		memset(arguments, 0, sizeof(arguments));
 		if (isatty(STDIN_FILENO) != 0)
 			printf("$ ");
+
 		read = getline(&line, &len, stdin);
 		if (read < 0)
 			break;
+
 		get_arguments(line, arguments);
 		if (arguments[0] == NULL)
 			continue;
+
 		if (arguments[0][0] != '/' && arguments[0][0] != '.')
 		{
 			arguments[0] = search_path_list(arguments[0], path_list);
-			status = 1;
-		}
-		code = process_command(arguments, env, argv);
-		if (status == 1)
+			status = process_command(arguments, env, argv);
 			free(arguments[0]);
-		if (code != 0)
+		}
+		else
+			status = process_command(arguments, env, argv);
+
+		if (status != 0)
 			break;
 	}
+
 	free(line);
 	free_list(path_list);
-	if (code == 127)
-		exit(127);
-	else if (code == -1)
-		exit(-1);
-	return (0);
+	exit(status);
 }
